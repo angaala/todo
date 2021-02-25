0b5cd729227960719e1d473a503bf75cb5494b26
angaala <menos009@gmail.com>
2021-02-26 01:58:01 +0500

    final version

--- js/todo.js
+++ js/todo.js
@@ -3,9 +3,35 @@ const button = document.querySelector('.add');
 const list = document.querySelector('.list');
 
 function createTask(value) {
-    task = document.createElement('div');
-    task.textContent = value;
-    task.className  = 'list-item';
+    const task = document.createElement('div');
+    const taskText = document.createElement('div');
+    const inputBlock = document.createElement('div');
+    const checkbox = document.createElement('input');
+    const deleteButton = document.createElement('input');
+
+    task.classList.add('list-item', 'non-success');
+
+    taskText.classList.add('task-text');
+    taskText.textContent = value;
+
+    inputBlock.classList.add('input-block');
+
+    checkbox.type = 'checkbox';
+    checkbox.classList.add('status', 'form-check-input');
+
+    deleteButton.type = 'button';
+    deleteButton.classList.add('btn', 'btn-primary', 'delete');
+    deleteButton.value = '×';
+
+    inputBlock.appendChild(checkbox);
+    inputBlock.appendChild(deleteButton);
+
+    checkbox.addEventListener("click", completeTask);
+    deleteButton.addEventListener("click", deleteTask);
+
+    task.appendChild(taskText);
+    task.appendChild(inputBlock);
+
     return task;
 }
 
@@ -18,3 +44,20 @@ function addTask() {
 }
 
 button.addEventListener("click", addTask);
+
+function completeTask(e) {
+    const inputBlock = e.target.parentElement;
+    const taskText = inputBlock.parentElement;
+    if(taskText.classList.contains('completed')) {
+        taskText.classList.remove('completed');
+    } else {
+        taskText.classList.add('completed');
+    }
+    
+}
+
+function deleteTask(e) {
+    const inputBlock = e.target.parentElement;
+    const task = inputBlock.parentElement;
+    task.parentElement.removeChild(task);
+}
